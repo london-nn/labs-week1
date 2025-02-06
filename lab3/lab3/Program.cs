c99e528322bf09c80d579d925326b6f3a82aa6e6
london-nn <edinorog521@gmail.com>
2025-02-06 18:32:47 +0500

    Add files via upload

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿class lab3
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FuelConsumption(60).ToString());
+    }
+
+    static double FuelConsumption(double fuel)
+    { 
+        double fuelConsumption = 7.5;
+        double distance = (fuel / fuelConsumption) * 100;
+
+        return distance;
+    }
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,18 @@
+﻿class lab4
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CurrencyExchange(100) + " tenge");
+    }
+
+    static double CurrencyExchange(int dollars)
+    {
+        int ExchangeRate = 475;
+        double Comission = 1.5;
+
+        double Exchange = (dollars * ExchangeRate);
+        double percent = Exchange * Comission / 100;
+
+        return Exchange - percent ;
+    }
+}
