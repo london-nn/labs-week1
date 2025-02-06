9e67514b9fa4347cb10e1b105c486031c4fdb7d1
london-nn <edinorog521@gmail.com>
2025-02-06 18:21:38 +0500

    Add files via upload

---
+++ лаба/лаба/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MinCookingTime(5).ToString());
+        Console.WriteLine(MinCookingTime(7).ToString());
+        Console.WriteLine(MinCookingTime(9).ToString());
+    }
+
+    static int MinCookingTime(int steaks)
+    {
+
+        int fullCookingTime = 6;
+        int halfCookingTime = 3;
+        double pan = 4;
+
+
+        double SwapCount = (steaks - 1) / pan;
+        if(steaks == 9) { SwapCount = 3; }
+        else { Math.Ceiling(SwapCount); }
+        
+        
+        int minCookingTime = Convert.ToInt32(SwapCount) * halfCookingTime + fullCookingTime;
+
+        return minCookingTime;
+    }
+}
