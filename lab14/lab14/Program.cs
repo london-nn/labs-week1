93977bea2d3a48455c1542a51e4e6804bab6cc1c
london-nn <edinorog521@gmail.com>
2025-02-06 18:46:57 +0500

    Add files via upload

---
+++ lab14/lab14/Program.cs
@@ -0,0 +1,21 @@
+﻿class lab14
+{
+    static void Main(string[] args)
+    {
+        AveageSpeedOfCar();
+    }
+
+    static void AveageSpeedOfCar()
+    {
+        double firstDistance = 150;
+        double firstTime = 3;
+        double firstSpeed = firstDistance / firstTime;
+        double secondDistance = 120;
+        double secondSpeed = firstSpeed * 1.15;
+        double secondTime = secondDistance / secondSpeed;
+        double totalDistance = firstDistance + secondDistance;
+        double totalTime = firstTime + secondTime;
+        double averageSpeed = totalDistance / totalTime;
+        Console.WriteLine("Average Speed: " + averageSpeed);
+    }
+}
---
+++ lab15/lab15/Program.cs
@@ -0,0 +1,19 @@
+﻿class lab15
+{
+    static void Main(string[] args)
+    {
+        ChocolateDivision();
+    }
+
+    static void ChocolateDivision()
+    {
+        int totalBars = 4;
+        int piecesPerBar = 28;
+        int totalPieces = totalBars * piecesPerBar;
+        int piecesForUser = 5;
+        int piecesToShare = totalPieces - piecesForUser;
+        int friends = 7;
+        int piecesPerFriend = piecesToShare / friends;
+        Console.WriteLine(piecesPerFriend + " piece per friend");
+    }
+}
